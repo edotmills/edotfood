ebfda6a8befb9bad9d4b8c92c6c0331cff0a7501
edotmills <edotmills@gmail.com>
2018-10-30 09:02:55 -0400

    Initial Commit

---
+++ edotfoodTests/edotfoodTests.swift
@@ -0,0 +1,34 @@
+//
+//  edotfoodTests.swift
+//  edotfoodTests
+//
+//  Created by Eric Mills on 10/30/18.
+//  Copyright © 2018 edotmills llc. All rights reserved.
+//
+
+import XCTest
+@testable import edotfood
+
+class edotfoodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
